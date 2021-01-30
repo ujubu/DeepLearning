11eb3e3b0c0109ad68674880f7e123e6c9e062d0
Yusuf Ziya <37000658+ujubu@users.noreply.github.com>
2021-01-30 20:49:27 +0300

    Add files via upload

---
+++ LinearRegression with Pytorch/LinearRegression.py
@@ -0,0 +1,56 @@
+#Linear Regresion with Pytorch DeepLearning
+import numpy as np
+import torch
+
+# Input (temp, rainfall, humidity)
+inputs = np.array([[73, 67, 43], 
+                   [91, 88, 64], 
+                   [87, 134, 58], 
+                   [102, 43, 37], 
+                   [69, 96, 70]], dtype='float32')
+
+# Targets (apples, oranges)
+targets = np.array([[56, 70], 
+                    [81, 101], 
+                    [119, 133], 
+                    [22, 37], 
+                    [103, 119]], dtype='float32')
+
+# Convert inputs and targets to tensors
+inputs = torch.from_numpy(inputs)
+targets = torch.from_numpy(targets)
+print("Printing inputs and outputs")
+print(inputs)
+print(targets)
+
+# Weights and biases
+w = torch.randn(2, 3, requires_grad=True)
+b = torch.randn(2, requires_grad=True)
+print("\nPrinting weights and bias")
+print(w)
+print(b)
+
+def model(x):
+    return x @ w.t() + b
+
+# MSE loss
+def mse(t1, t2):
+    diff = t1 - t2
+    return torch.sum(diff * diff) / diff.numel()
+
+# Train for 100 epochs
+for i in range(100):
+    preds = model(inputs)
+    loss = mse(preds, targets)
+    loss.backward()
+    with torch.no_grad():
+        w -= w.grad * 1e-5
+        b -= b.grad * 1e-5
+        w.grad.zero_()
+        b.grad.zero_()
+
+# Compare with prediction and true values
+print("\nPrinting prediction and true values")
+print(preds)
+print(targets)
+
